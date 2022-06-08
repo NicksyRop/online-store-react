9a54d1859d121cdd30a365527955a00bae53f98a
NicksyRop <nicksonkipkorir25@gmail.com>
2022-06-08 12:48:16 +0300

    added two route for home and cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/about">
+            <Cart />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
