49a4159c94b8ecf0bb10b19bf9b0380734ac095f
NicksyRop <nicksonkipkorir25@gmail.com>
2022-06-08 20:27:34 +0300

    loaded  data from back end api and displayed them in the Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,32 @@ import { useGetAllProductsQuery } from "../features/productApi";
 const Home = () => {
   const { data, error, isLoading } = useGetAllProductsQuery();
 
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      {isLoading ? (
+        <p>Loading</p>
+      ) : error ? (
+        <p>An error occured</p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className="product">
+            {data?.map((item) => (
+              <div key={item.id} className="product">
+                <h3>{item.name}</h3>
+                <img alt="product pictures" src={item.image} />
+                <div className="details">
+                  <span className="desc">{item.desc}</span>
+                  <span className="price">${item.price}</span>
+                </div>
+                <button>Add To Cart</button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
