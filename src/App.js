e5be070c9b15f8677a5971aa307ed29cc52b8310
NicksyRop <nicksonkipkorir25@gmail.com>
2022-06-08 13:26:45 +0300

    Styled the navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <NavBar />
         <Switch>
-          <Route path="/about">
+          <Route path="/cart">
             <Cart />
           </Route>
 
