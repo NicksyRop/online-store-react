c8b39124c59176c8ff177b058ef4c296baa50dcd
NicksyRop <nicksonkipkorir25@gmail.com>
2022-06-22 10:07:58 +0300

    changed RTK query baseUrl

--- src/features/productApi.js
+++ src/features/productApi.js
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productApi = createApi({
   reducerPath: "productApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://react-online-shop-api.herokuapp.com/",
+  }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => "products",
